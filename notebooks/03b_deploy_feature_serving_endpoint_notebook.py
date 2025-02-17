fe30972b93f5f85ff278aaf2b10d3bbf1d1d919d
Mehmet Acikgoz <acikgozmm@gmail.com>
2025-02-17 13:47:47 -0600

    update feature_serving

--- notebooks/03b_deploy_feature_serving_endpoint_notebook.py
+++ notebooks/03b_deploy_feature_serving_endpoint_notebook.py
@@ -12,6 +12,7 @@ from IPython.core.display_functions import display
 import os
 import pathlib
 
+import time
 import pandas as pd
 import requests
 from dotenv import load_dotenv
@@ -34,10 +35,11 @@ spark = SparkSession.builder.getOrCreate()
 dbutils = DBUtils(spark)
 
 # COMMAND ----------
+# get environment variables
 os.environ['DBR_TOKEN'] = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
 os.environ['DBR_HOST'] = spark.conf.get('spark.databricks.workspaceUrl')
-# COMMAND ----------
 
+# COMMAND ----------
 mlflow.set_tracking_uri("databricks")
 mlflow.set_registry_uri("databricks-uc")
 
@@ -45,10 +47,10 @@ fe = feature_engineering.FeatureEngineeringClient()
 
 
 # COMMAND ----------
+# Load project env
 envfile_path=pathlib.Path().joinpath("../project.env").resolve().as_posix()
 print(f'{envfile_path =}')
 
-# COMMAND ----------
 load_dotenv(envfile_path)
 
 DEPLOYMENT_LOGS = os.environ['DEPLOYMENT_LOGS']
@@ -56,59 +58,54 @@ DEPLOYMENT_LOGS = pathlib.Path(DEPLOYMENT_LOGS).resolve().as_posix()
 print(f"{DEPLOYMENT_LOGS = }")
 
 # COMMAND ----------
-
+# logging
 setup_logging(DEPLOYMENT_LOGS)
 
 # COMMAND ----------
 if is_databricks():
     CONFIG_FILE_PATH = pathlib.Path("../project_config.yml").resolve().as_posix()
 
-
 print(f"{CONFIG_FILE_PATH = }")
 
 # COMMAND ----------
 CONFIG = Config.from_yaml(CONFIG_FILE_PATH)
-tags = Tags(branch="dev")
-
 
 # COMMAND ----------
 # configuration changed for testing
-# configuration changed for testing
-CONFIG.experiment_name = CONFIG.experiment_name + "-basic"
 CONFIG.model.name = CONFIG.model.name + "_basic"
-tags = Tags(branch="dev")
-
-logger.info(f"{CONFIG.experiment_name = }")
 logger.info(f"{CONFIG.model.name = }")
-logger.info(f"{CONFIG.model.artifact_path = }")
 
 # COMMAND ----------
 catalog_name = CONFIG.catalog_name
 schema_name = CONFIG.schema_name
-feature_table_name = f"{catalog_name}.{schema_name}.hotel_reservations_preds"
-feature_spec_name = f"{catalog_name}.{schema_name}.return_predictions"
+model_name = CONFIG.model.name
+full_model_name=f"{CONFIG.catalog_name}.{CONFIG.schema_name}.{CONFIG.model.name}"
+
+logger.info(f"{catalog_name = }")
+logger.info(f"{schema_name = }")
+logger.info(f"{model_name = }")
+logger.info(f"{full_model_name = }")
 
 # COMMAND ----------
-# Initialize model
-# full_model_name=f"{CONFIG.catalog_name}.{CONFIG.schema_name}.{CONFIG.model.name}"
-# print(f"{full_model_name = }")
-
+feature_table_name = f"{catalog_name}.{schema_name}.hotel_reservations_preds"
+feature_spec_name = f"{catalog_name}.{schema_name}.return_predictions"
 endpoint_name = CONFIG.model.name.replace("_", "-") +"-feature-serving"
-print(f"{endpoint_name = }")
+
+logger.info(f"{feature_table_name = }")
+logger.info(f"{feature_spec_name = }")
+logger.info(f"{endpoint_name = }")
 
 # COMMAND ----------
 train_set = spark.table(f"{catalog_name}.{schema_name}.train_set").toPandas()
 test_set = spark.table(f"{catalog_name}.{schema_name}.test_set").toPandas()
 df = pd.concat([train_set, test_set])
 
-model = mlflow.sklearn.load_model(f"models:/{catalog_name}.{schema_name}.hotel_reservations_model_basic@latest-model")
-
+model = mlflow.sklearn.load_model(f"models:/{catalog_name}.{schema_name}.{model_name}@latest-model")
 
 # COMMAND ----------
-preds_df = df[["booking_id"]]
+preds_df = df[["booking_id","repeated_guest","no_of_previous_cancellations","no_of_previous_bookings_not_canceled"]]
 preds_df["Predicted_Default"] = model.predict(df[CONFIG.features.categorical + CONFIG.features.numerical])
 preds_df = spark.createDataFrame(preds_df)
-
 
 # COMMAND ----------
 fe.create_table(
@@ -118,12 +115,12 @@ fe.create_table(
   description = "Hotel booking cancellation predictions"
 )
 
-
 # COMMAND ----------
 spark.sql(f"ALTER TABLE {feature_table_name} "
           "SET TBLPROPERTIES (delta.enableChangeDataFeed = true);")
 
 # COMMAND ----------
+# Initialize feature store manager
 feature_serving = FeatureServing(
     feature_table_name=feature_table_name, feature_spec_name=feature_spec_name, endpoint_name=endpoint_name
 )
@@ -137,19 +134,48 @@ feature_serving.create_feature_spec()
 
 # COMMAND ----------
 # Deploy feature serving endpoint
-feature_serving.deploy_or_update_serving_endpoint_with_retry()
-
-# COMMAND ----------
+feature_serving.deploy_or_update_serving_endpoint_with_retry(retry_interval=60)
 
 # COMMAND ----------
 dataframe_records = [{"booking_id": "INN32499"}]
-print(dataframe_records)
-# COMMAND ----------
-status_code, response_text = call_endpoint(endpoint_name, dataframe_records)
-print(f"Response Status: {status_code}")
-print(f"Response Text: {response_text}")
+logger.info(dataframe_records)
 
 # COMMAND ----------
+start_time = time.time()
+status_code, response_text = call_endpoint(endpoint_name, dataframe_records)
+end_time = time.time()
+execution_time = end_time - start_time
+
+logger.info("Response status:", status_code)
+logger.info("Response text:", response_text)
+logger.info("Execution time:", execution_time, "seconds")
+
+# COMMAND ----------
+serving_endpoint = f"https://{os.environ['DBR_HOST']}/serving-endpoints/{endpoint_name}/invocations"
+logger.info(f"Serving Endpoint: {serving_endpoint}")
+
+response = requests.post(
+    f"{serving_endpoint}",
+    headers={"Authorization": f"Bearer {os.environ['DBR_TOKEN']}"},
+    json={"dataframe_split": {"columns": ["booking_id"], "data": [["INN32499"]]}},
+)
+
+# COMMAND ----------
+logger.info("Response status:", response.status_code)
+logger.info("Response text:", response.text)
+logger.info("Execution time:", execution_time, "seconds")
+
+# COMMAND ----------
+# delete the endpoint
+feature_serving.delete_serving_endpoint()
+
+# COMMAND ----------
+#delete feature_spec
+feature_serving.delete_feature_spec()
+
+# COMMAND ----------
+
+
 # delete feature spec
 # from databricks.feature_engineering import FeatureEngineeringClient
 
